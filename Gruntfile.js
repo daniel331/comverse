51345b1eba49252e81ee3f3c3ceaec4acb09f426
daniel331 <daniel331@gmail.com>
2015-03-27 20:53:34 +0300

    HW1 finish
    Added a grunt task to conver md to html.
    Generated an html file (it has some styling and stuff we don't need, but
    it's a good start).

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,6 +12,18 @@ module.exports = function (grunt) {
 				src: 'src/js/main.js',
 				dest: 'dist/js/main.min.js'
 			}
+		},
+		markdown: {
+		    all: {
+		      files: [
+		        {
+		          expand: true,
+		          src: 'index.md',
+		          dest: 'html/',
+		          ext: '.html'
+		        }
+		      ]
+		    }
 		}
 	});
 
@@ -19,7 +31,8 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-markdown');
 
 	// Default task
-	grunt.registerTask('default', ['uglify']);
+	grunt.registerTask('default', ['uglify', 'markdown']);
 };
